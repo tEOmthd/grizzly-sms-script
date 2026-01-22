0b056de79de482fbc09f12ed067c485340450da7
teo mathiaud <teo.mathiaud@etu.univ-amu.fr>
2026-01-22 13:50:24 +0100

    Ajout du fichier de configuration pour Grizzly SMS et mise à jour des dépendances dans requirements.txt

---
+++ config.py
@@ -0,0 +1,34 @@
+"""
+Configuration pour Grizzly SMS
+Obtenez votre clé API sur: https://grizzlysms.com
+"""
+
+# ⚠️ IMPORTANT: Remplacez par votre vraie clé API
+API_KEY = "votre_clé_api_ici"
+
+# Pays disponibles (codes à 2 lettres)
+COUNTRIES = {
+    'fr': 'France',
+    'us': 'USA',
+    'uk': 'Royaume-Uni',
+    'de': 'Allemagne',
+    'ru': 'Russie',
+    'es': 'Espagne',
+    'it': 'Italie',
+    'ca': 'Canada',
+    'be': 'Belgique',
+    'ch': 'Suisse'
+}
+
+# Services populaires
+SERVICES = {
+    'other': 'Usage général (le moins cher)',
+    'google': 'Google / Gmail',
+    'whatsapp': 'WhatsApp',
+    'telegram': 'Telegram',
+    'facebook': 'Facebook',
+    'instagram': 'Instagram',
+    'uber': 'Uber',
+    'twitter': 'Twitter / X',
+    'tiktok': 'TikTok'
+}
